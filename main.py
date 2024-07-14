973614dd98347bd8fe6c1eededaf6be931c91f09
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-07-14 14:43:04 +0500

    Added entry point to application main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from Typing_Tester.integrator import run_typing_test_application
+
+
+if __name__ == '__main__':
+
+    run_typing_test_application('9d8b2b5af0msh86cec870d63f592p1e21e9jsn1933be69eade')
