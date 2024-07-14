c789c6a974beb7c553a78c057c76b745be258625
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-07-14 12:35:55 +0500

    Implemented run_typing_application function in integrator.py module for testing that all modules functioning well after integration

---
+++ Typing_Tester/integrator.py
@@ -0,0 +1,31 @@
+
+from .typingtest import TypingTestManager
+from .score_calculator import TypingTestCalculator
+from .report_generator import TypingTestReport
+from .random_word_fetcher import RandomWordFetcher
+
+
+def run_typing_test_application(api_key: str):
+    # Initialize the necessary components
+    word_fetcher = RandomWordFetcher(api_key)
+    typing_test_manager = TypingTestManager()
+    score_calculator = TypingTestCalculator()
+    report_generator = TypingTestReport()
+
+    # Fetch a random word
+    fetched_word = word_fetcher.fetch_word()
+
+    # Start the typing test
+    typing_test_session = typing_test_manager.start_typing_test(fetched_word.word)
+
+    # Calculate the score
+    typing_test_result = score_calculator.compute_total_score(
+        typing_test_session.time_taken,
+        typing_test_session.user_typed_word,
+        fetched_word,
+        typing_test_session.get_back_spaces_count()
+    )
+
+    # Generate and display the report
+    report = report_generator.generate_report(typing_test_result)
+    print(report)
