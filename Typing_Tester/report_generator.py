6072ed459a46947e99eef681ba067429455de004
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-08-21 15:50:07 +0500

    added docstring

--- Typing_Tester/report_generator.py
+++ Typing_Tester/report_generator.py
@@ -2,7 +2,9 @@ from Typing_Tester.typing_test_result import TypingTestResult
 
 
 class TypingTestReport:
-
+    """
+        Class responsible for generating reports based on the typing test results.
+    """
     @staticmethod
     def generate_report(score_result: TypingTestResult):
 
